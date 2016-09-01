7e2511d5569f5cb6d7f8fb9b57b9738c5d671cd5
CptJohnnyWalker <pyleben1776@gmail.com>
2016-09-01 15:02:01 -0400

    Adds Ben changes

--- bot.py
+++ bot.py
@@ -3,87 +3,86 @@ import asyncio
 import random
 import json
 import cleverbot
+import json
+from urllib.request import urlopen
 
-cb1 = cleverbot.Cleverbot()
-cb2 = cleverbot.Cleverbot()
-cb3 = cleverbot.Cleverbot()
-cb4 = cleverbot.Cleverbot()
-cb5 = cleverbot.Cleverbot()
+#########################################################
+#Globals
 
+cb = cleverbot.Cleverbot()
 client = discord.Client()
-messages_db = []
+hypedb = ["http://i.imgur.com/1oNSmyl.gif",
+          "http://gifs.benlk.com/happening.gif",
+          "https://31.media.tumblr.com/4631d343dd011414e886977c73a6bb03/tumblr_n58fqtFyTj1svlwhbo1_1280.gif",
+          "http://s3.amazonaws.com/rapgenius/funny-gif-Colbert-screaming.gif",
+          "https://fat.gfycat.com/ActualFeistyBettong.gif",
+          "http://1.bp.blogspot.com/-V0lGJz82ijw/UsnKIAqEp1I/AAAAAAAAA6E/mdmU5rJwBGw/s1600/RIVAHHSSSHYPED.gif",
+          "https://49.media.tumblr.com/4581c0f0a529da432bf5ac84e3d5de0a/tumblr_ncj65sNEnd1sr6y44o1_500.gif"
+          ]
+rossb = ["http://3.bp.blogspot.com/-XxbTk4t1bpA/UI_PJi1BABI/AAAAAAAABgg/tnOiIsB2dKY/s1600/bob_ross_csg024_autumn_glory.jpg", 
+         "http://artforsaleamerica.com/paintings-image/bob-ross/bob-ross-sun-after-rain-86142.jpg",
+         "http://1.bp.blogspot.com/-YujIJyDFHLE/UI_Ovw_VuFI/AAAAAAAABfw/XWMIIKg_9PI/s1600/bob_ross_csg015_twilight_meadow.jpg",
+         "http://2.bp.blogspot.com/_pT-Sqkrybuk/TG5Xo7l2hBI/AAAAAAAAAGY/bLY1zmnl7oQ/s1600/bob-ross-landscape-painting-281-2.jpg",
+         "http://www.deshow.net/d/file/cartoon/2008-12/bob-ross-oil-painting-282-46.jpg",
+         "http://img.xcitefun.net/users/2009/03/41037,xcitefun-bob-ross-paintings-beautiful-4.jpg",
+         "http://4.bp.blogspot.com/-pkpOG2oWDi0/UJL0PbUpEnI/AAAAAAAAGz0/aAslDaF1M2o/s1600/bob+ross+1.jpg",
+         "http://1.bp.blogspot.com/-0tPGQRWyrV8/UJL0dsrq-6I/AAAAAAAAG08/TdSulvFVmik/s1600/bob+ross+18.jpg",
+         "http://3.bp.blogspot.com/-a_UGVnmjghk/UajazIi6eyI/AAAAAAAAC6U/VDAuzdUg81Y/s1600/bob-ross-landscape-oil-painting-27-18.jpg",
+         "http://www.saleoilpaintings.com/paintings-image/bob-ross/bob-ross-natures-grandeur-86102.jpg",
+         "http://www.saleoilpaintings.com/paintings-image/bob-ross/bob-ross-purple-haze-86115.jpg",
+         "http://www.saleoilpaintings.com/paintings-image/bob-ross/bob-ross-mountain-sunset-86095.jpg"
+         ]
 
-async def conversation_interface(message):
-    cb = cb5
-    user = str(message.author)
-    if user == "Maurice":
-        cb = cb1
-        print(user)
-    if user == "Captain Johnny Walker":
-        cb = cb2
-        print(user)
-    if user == "Baughb42":
-        cb = cb3
-        print(user)
-    if user == "Bigbidoof":
-        cb = cb4
-        print(user)
+#########################################################
+#Helper fucntions
 
-    question = str(message.content)[22:]
-    print(question)
-    msg = cb.ask(question)
-    await client.send_message(message.channel, msg)
-
-async def command_interface(message):
-    command = str(message.content).split()
-
-    if command[1] == "quit":
-        with open("database", 'w') as output:
-            json.dump(messages_db, output)
-
-        client.close()
-        client.logout()
-
-    if command[1] == "quote":
-        message_list = []
-        for user in messages_db:
-            if user[1] == command[2]:
-                message_list.append(user)
-        for user in message_list:
-            if user[0] == command[3]+ ' ' + command[4]:
-                msg = "%s said, \"%s\" on %s" % (user[1], user[2], user[0])
-                await client.send_message(message.channel, msg)
-
-    if command[1] == "dice":
-        sidesn = int(command[2])
-        msg = random.randrange(1,sidesn,1)
-        await client.send_message(message.channel, msg)
-
-    if command[1] == "help":
-        await client.send_message(message.channel, "quote name yyyy-mm-dd 24hr:time")
-        await client.send_message(message.channel, "dice sides")
-
-
+async def AppendId(message, msg):
+    msg += "<@%s> " % (message.author.id)
+    return msg
+#########################################################
+#Events
 
 @client.event
 async def on_ready():
     print(client.user.name)
-    with open("database", 'w') as input_f:
-        messages_db = json.dumps(input_f)
 
+    
 @client.event
 async def on_message(message):
+    if client.user.id in message.content:
+        await MessageParse(message)
 
-    message_info = [str(message.timestamp)[0:16], str(message.author), str(message.content)]
-    messages_db.append(message_info)
+async def MessageParse(message):
+    message_array = str(message.content).split()
 
-    print(message_info)
+    if message_array[1] == "!hype":
+        await Hype(message)
+    elif message_array[1] == "!ross":
+              await Ross(message)
+    else:
+        await Cleverbot(message)
 
-    if message.content.startswith('!command'):
-        await command_interface(message)
-    if message.content.startswith('<@157179616816136201>'):
-        await conversation_interface(message)
+##########################################################
+#Actions
+async def Cleverbot(message):
+    question = str(message.content)[22:]
+    msg = ''
+    msg = await AppendId(message, msg)
+    msg += cb.ask(question)
+    await client.send_message(message.channel, msg)
 
-
-
-client.run("", "")
+async def Hype(message):
+    msg = ''
+    msg = await AppendId(message, msg)
+    msg += random.choice(hypedb)
+    await client.send_message(message.channel, msg)
+    
+async def Ross(message):
+    msg = ''
+    msg = await AppendId(message, msg)
+    msg += random.choice(rossb)
+    await client.send_message(message.channel, msg)
+##########################################################
+#Connection
+               
+client.run("pinkfloyd6000@gmail.com", "bono01")
